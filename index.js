7c8851aa8b0df923fbd3575191b917d26f747def
ericanvoued <ericanvoued@gemail.com>
2019-08-25 18:35:16 +0800

    代码玉华

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 
 const jquery = require("jquery");
 const jsdom = require("jsdom");
-
+let es = null;
 
 let getHotNews = res => {
   let hotNews = { title: {}, data: [] };
@@ -51,28 +51,52 @@ app.get("/", async (req, res, next) => {
         var resBody = iconv.decode(Buffer.from(r.body), "gb2312");
         //数据提取格式化
         let formatBody = getHotNews(resBody);
-        res.send(formatBody);2
+        
         //通过工具将json转表对象
-        let ss = xlsx.utils.json_to_sheet(formatBody.data); 
+        let target = xlsx.utils.json_to_sheet(formatBody.data); 
         //定义表头
-        ss.A1.v = formatBody.title.date;
-        ss.B1.v = formatBody.title.status;
-        ss.C1.v = formatBody.title.code;
-        ss.D1.v = formatBody.title.winddiretion;
-        let keys = Object.keys(ss).sort();
-        let ref = ss["!ref"]; //定义表范围
-        let workbook = {
-          //定义操作文档
-          SheetNames: ["nodejs-sheetname"], //定义表明
-          Sheets: {
-            "nodejs-sheetname": Object.assign({}, ss, { "!ref": ref }) //表对象
+        target.A1.v = formatBody.title.date;
+        target.B1.v = formatBody.title.status;
+        target.C1.v = formatBody.title.code;
+        target.D1.v = formatBody.title.winddiretion;
+        let keys = Object.keys(target).sort();
+        let ref = target["!ref"]; //定义表范围
+        let filename = `./清远天气数据.xls`;
+        try{
+          es = xlsx.readFile('./清远天气数据.xls', {cellStyles: true})
+        }catch(error) {
+          
+        }
+        if(es == null) {
+          let workbook = {
+            //定义操作文档
+            SheetNames: [month], //定义表明
+            Sheets: {
+              [month]: Object.assign({}, target, { "!ref": ref }) //表对象
+            }
+          };
+          xlsx.writeFile(workbook, filename); //将数据写入文件
+          res.send(formatBody);
+          console.log('创建成功')
+        }else {
+          if(es.SheetNames.indexOf(month) > -1) {
+            console.log("本地已有改月份数据，无需再创建")
+            res.send(formatBody);
+            return
           }
-        };
-        let filename = `./${month}.xls`;
-        xlsx.writeFile(workbook, filename); //将数据写入文件
+          es.SheetNames.push(month);
+          es.Sheets[month] = Object.assign({}, target, { "!ref": ref }) //表对象
+          es.SheetNames.sort();
+          Object.keys(es.Sheets).sort();
+          xlsx.writeFile(es, filename); //将数据写入文件
+          res.send(formatBody);
+          console.log('创建成功')
+        }
       }
     });
 });
+
+
 
 let server = app.listen(3000, function() {
   let host = server.address().address;
